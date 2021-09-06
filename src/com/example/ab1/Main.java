37aef36822b070f04e08f8952fbe7bfbb9a5dd5c
systec24 <parkjosh1234@gmail.com>
2021-09-06 23:56:04 +0900

    1st Commit

---
+++ src/com/example/ab1/Main.java
@@ -0,0 +1,14 @@
+package com.example.ab1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello Worrld!!!\n");
+		System.out.print("이름 입력 : ");
+		String name = s.next();
+		System.out.println("[" + name + "]님 환영합니다~");
+	}
+}
